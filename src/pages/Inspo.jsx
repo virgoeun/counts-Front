52dda044b16dbe1bc478ae1046498cb41b66eb74
Alice <johndoe@example.com>
2023-09-30 16:45:55 +0200

    half done

--- src/pages/Inspo.jsx
+++ src/pages/Inspo.jsx
@@ -5,129 +5,106 @@ const API_URL = "http://localhost:5005";
 import React, { useEffect, useState } from "react";
 
 // const InspirationPage = () => {
-//   const [instagramPosts, setInstagramPosts] = useState([]);
+//   const [spotifyPlaylist, setspotifyPlaylist] = useState([]);
 
 //   const storeToken = (token) => {
 //     localStorage.setItem("authToken", token);
-//   }
+//   };
 
-//     const storedToken = localStorage.getItem("authToken");
-//     console.log("Token", storedToken)
+//   const storedToken = localStorage.getItem("authToken");
+//   console.log("Token", storedToken);
 
 //   useEffect(() => {
 //     // Make the API request to get Instagram posts
+
+//     console.log("Authorization Header:", `Bearer ${storedToken}`);
+
 //     axios
-//       .get(`${API_URL}/api/inspiration`, {headers: {
-//         Authorization: `Bearer ${storedToken}` // Make sure storedToken contains a valid JWT
-//       },
-//     })
+//       .get(`${API_URL}/api/inspiration`, {
+//         headers: {
+//           Authorization: `Bearer ${storedToken}`, // Make sure storedToken contains a valid JWT
+//         },
+//       })
 //       .then((response) => {
-//         setInstagramPosts(response.data);
-//         console.log("Instagram info:", response.data);
+//         setspotifyPlaylist(response.data.playlists.items);
+//         console.log("Spotify Data:", response.data.playlists.items);
 //       })
 //       .catch((error) => {
-//         console.error("Error fetching Instagram posts:", error);
+//         console.error("Error fetching Spotify Data:", error);
 //       });
 //   }, []);
 
 //   return (
 //     <div>
 //       <h1>Inspiration Page</h1>
-//       {instagramPosts.length > 0 ? (
+//       {spotifyPlaylist.length > 0 ? (
 //         <div>
-//           {/* Map over Instagram posts and render each item */}
-//           {instagramPosts.map((post, index) => (
+//           {spotifyPlaylist.slice(0, 5).map((playlist, index) => (
 //             <div key={index}>
-//               <p>Post:</p>
-
+//               <h2>{playlist.data.name}</h2>
+//               {/* <p>Description: {playlist.data.description}</p>
+//               <p>Owner: {playlist.data.owner.name}</p> */}
 //               <button
-//           onClick={() => {
-//             window.open(post.node.display_url, '_blank');
-//           }}
-//         >
-//           Open Post
-//         </button>
-//               <Link to={post.node.display_url} target="_blank">
-//           <img src={post.node.display_url} alt={`Post ${index}`}
-//           // onError={(e) => {
-//           //   e.target.src = "../assets/react.svg"; // Replace with a fallback image URL
-//           // }}
-//           />
-//         </Link>
-//         <p>Video Post:</p>
-//         {/* Use Link component for the "Video" link */}
-//         <Link to={post.node.video_url} target="_blank">
-//           <img src={post.node.thumbnail_src} alt={`Video ${index}`} />
-//         </Link>
-//               {/* <img src={post.node.thumbnail_src} /> */}
+//                 onClick={() => {
+//                   window.open(playlist.data.uri, "_blank");
+//                 }}
+//               >
+//                 Check on Spotify 🎧
+//               </button>
+//               <img
+//                 src={playlist.data.images.items[0].sources[0].url}
+//                 alt={`Playlist ${index}`}
+//               />
 //             </div>
 //           ))}
 //         </div>
 //       ) : (
-//         <p>Loading Inspiration ...</p>
+//         <p>Loading Spotify Playlist ...🥁🎶</p>
 //       )}
 //     </div>
 //   );
 // };
 
-const InspirationPage = () => {
-  const [spotifyPlaylist, setspotifyPlaylist] = useState([]);
+// const InspirationPage = () => {
+//   const [datascrapper, setDataScrapper] = useState([]);
 
-  const storeToken = (token) => {
-    localStorage.setItem("authToken", token);
-  };
+//   const storeToken = (token) => {
+//     localStorage.setItem("authToken", token);
+//   };
 
-  const storedToken = localStorage.getItem("authToken");
-  console.log("Token", storedToken);
+//   const storedToken = localStorage.getItem("authToken");
+//   console.log("Token", storedToken);
 
-  useEffect(() => {
-    // Make the API request to get Instagram posts
+//   useEffect(() => {
+//     // Make the API request to get Instagram posts
 
-    console.log("Authorization Header:", `Bearer ${storedToken}`);
+//     console.log("Authorization Header:", `Bearer ${storedToken}`);
 
-    axios
-      .get(`${API_URL}/api/inspiration`, {
-        headers: {
-          Authorization: `Bearer ${storedToken}`, // Make sure storedToken contains a valid JWT
-        },
-      })
-      .then((response) => {
-        setspotifyPlaylist(response.data.playlists.items);
-        console.log("Spotify Data:", response.data.playlists.items);
-      })
-      .catch((error) => {
-        console.error("Error fetching Spotify Data:", error);
-      });
-  }, []);
+//     axios
+//       .get(`${API_URL}/api/inspiration`, {
+//         headers: {
+//           Authorization: `Bearer ${storedToken}`, // Make sure storedToken contains a valid JWT
+//         },
+//       })
+//       .then((response) => {
+//         setDataScrapper(response.data);
+//         console.log("Scrapped Data:", response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching Scrapped Data:", error);
+//       });
+//   }, []);
 
-  return (
-    <div>
-      <h1>Inspiration Page</h1>
-      {spotifyPlaylist.length > 0 ? (
-        <div>
-          {spotifyPlaylist.slice(0, 5).map((playlist, index) => (
-            <div key={index}>
-              <h2>{playlist.data.name}</h2>
-              {/* <p>Description: {playlist.data.description}</p>
-              <p>Owner: {playlist.data.owner.name}</p> */}
-              <button
-                onClick={() => {
-                  window.open(playlist.data.uri, "_blank");
-                }}
-              >
-                Check on Spotify 🎧
-              </button>
-              <img
-                src={playlist.data.images.items[0].sources[0].url}
-                alt={`Playlist ${index}`}
-              />
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading Spotify Playlist ...🥁🎶</p>
-      )}
-    </div>
-  );
-};
-export default InspirationPage;
+//   return (
+//     <div>
+//       <h1>Inspiration Page</h1>
+//       {datascrapper.htmlInferred && (
+//         <div>
+//           <img src={datascrapper.htmlInferred.url}/>
+//           {/* Include other properties you want to render */}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+// export default InspirationPage;
