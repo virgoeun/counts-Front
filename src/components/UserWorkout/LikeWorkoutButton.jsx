1f377ffc0e5cc36e96fde610289bf84397567688
Alice <johndoe@example.com>
2023-10-04 01:43:59 +0200

    likes done

--- src/components/UserWorkout/LikeWorkoutButton.jsx
+++ src/components/UserWorkout/LikeWorkoutButton.jsx
@@ -3,8 +3,9 @@ import axios from "axios";
 
 function LikeWorkoutButton({ workoutId }) {
   const [liked, setLiked] = useState(false);
+  const storedToken = localStorage.getItem("authToken");
 
-
+  const API_URL = "http://localhost:5005";
 
   useEffect(() => {
     // Fetch initial like status when the component mounts
@@ -12,11 +13,12 @@ function LikeWorkoutButton({ workoutId }) {
   }, [workoutId]);
 
   const fetchLikeStatus = () => {
-
-
     axios
-      .get(`/api/workouts/${workoutId}/liked`)
+      .get(`${API_URL}/api/workout/${workoutId}/liked`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
       .then((response) => {
+        console.log(response.data);
         setLiked(response.data.liked);
       })
       .catch((error) => {
@@ -27,12 +29,37 @@ function LikeWorkoutButton({ workoutId }) {
   const handleLikeClick = () => {
     let requestPromise;
 
+    //without requestPromise...
+    // axios.post(
+    //     `${API_URL}/api/workouts/${workoutId}/liked`,
+    //     null, // Use null as the request body for a simple POST request
+    //     { headers }
+    //   )
+    //     .then(() => {
+    //       // Toggle the liked state after successful request
+    //       setLiked(!liked);
+    //     })
+    //     .catch((error) => {
+    //       console.error("Error liking/unliking workout:", error);
+    //     });
+
+    const headers = {
+      Authorization: `Bearer ${storedToken}`,
+    };
+
     if (liked) {
       // If already liked, unlike the workout
-      requestPromise = axios.delete(`/api/workouts/${workoutId}/liked`);
+      requestPromise = axios.delete(
+        `${API_URL}/api/workout/${workoutId}/liked`,
+        { headers }
+      );
     } else {
       // If not liked, like the workout
-      requestPromise = axios.post(`/api/workouts/${workoutId}/liked`);
+      requestPromise = axios.post(
+        `${API_URL}/api/workout/${workoutId}/liked`,
+        null, // Use null as the request body for a simple POST request
+        { headers }
+      );
     }
 
     requestPromise
